be13be62ba5478208dec7c292be20d58770a2a84
Charl du Plessis <cjdupless@gmail.com>
2024-02-21 17:28:52 +0200

    status and task, README changes

--- task.go
+++ task.go
@@ -2,46 +2,82 @@ package tflux
 
 import (
 	"fmt"
+	"sync"
 	"github.com/google/uuid"
 )
 
+type linkDirection bool
+
+const (
+	down = false
+	up   = true
+)
+
 type TaskResult struct {
-	ExtraInfo string
-	ExecResult interface{}
-	Status Status
+	Info  string
 	Error error
 }
 
-type ExecFunc func() TaskResult
-
 type Task struct {
-	id string
-	status Status 
-	executable ExecFunc
-	upstream map[*Task]bool	
+	id         string
+	status     Status
+	function   func() TaskResult
+	upstream   []*Task
 	downstream []*Task
 }
 
 func NewTask() *Task {
 	t := Task{
-		upstream: make(map[*Task]bool),
+		upstream:   make([]*Task, 0),
 		downstream: make([]*Task, 0),
 	}
 	t.status = NoneStatus
-	t.id = uuid.New().String()  
+	t.id = uuid.New().String()
 	return &t
 }
 
-func (t *Task) SetExec(exec ExecFunc) error {
-	if exec == nil {
-		return fmt.Errorf("the executable cannot be nil")
+func (t *Task) SetStatus(status Status) {
+	t.status = status
+	if t.status == FailedStatus || t.status == UpFailedStatus {
+		for _, task := range t.downstream {
+			task.SetStatus(UpFailedStatus)
+		}
 	}
-	t.executable = exec
+}
+
+func (t *Task) canRun() bool {
+	if t.status == UpFailedStatus {
+		return false
+	}
+	successCount := 0
+	for _, task := range t.upstream {
+		if task.status == SuccessStatus {
+			successCount++
+		}
+	}
+	return successCount == len(t.upstream)
+}
+
+func (t *Task) done() bool {
+	return (
+		t.status == FailedStatus || 
+		t.status == UpFailedStatus || 
+		t.status == SuccessStatus)
+}
+
+func (t *Task) Run() {
+	result := t.function()
+	if result.Error != nil {
+		// Do some logging.
+		t.SetStatus(FailedStatus)
+		return
+	}
+	t.status = SuccessStatus
 }
 
 func (t *Task) Clone() *Task {
 	clone := NewTask()
-	clone.executable = t.executable
+	clone.function = t.function
 	return clone
 }
 
@@ -49,22 +85,10 @@ func (t *Task) String() string {
 	return fmt.Sprintf("%v", *t)
 }
 
-func (t *Task) Run() {
-	// Pre-execution instructions
-	err := t.executable()
-	if err != nil {
-		// Do some logging.
-		t.status = FailedStatus
-		return
+func (t *Task) addLink(lt *Task, direction linkDirection) {
+	if direction == up {
+		t.upstream = append(t.upstream, lt)
+	} else {
+		t.downstream = append(t.downstream, lt)
 	}
-	t.status = SuccessStatus
-	// Post-execution instructions
-}
-
-func (t *Task) addDownstream(dt *Task) {
-	t.downstream = append(t.downstream, dt)
-}
-
-func (t *Task) addUpstream(ut *Task) {
-	t.upstream[ut] = true
 }
